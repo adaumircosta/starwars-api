a510d3ee793c9716c35a731f89886b73d97b4fa2
Adaumir Costa <adaumircosta@gmail.com>
2024-04-29 01:15:01 -0400

    implements new methods

--- src/main/java/br/com/starwars/api/external/repository/starwars/services/impl/RepositoryServiceImpl.java
+++ src/main/java/br/com/starwars/api/external/repository/starwars/services/impl/RepositoryServiceImpl.java
@@ -1,15 +1,32 @@
 package br.com.starwars.api.external.repository.starwars.services.impl;
 
-import br.com.starwars.api.domain.Species;
+import br.com.starwars.api.external.repository.starwars.FilmPersonRepository;
+import br.com.starwars.api.external.repository.starwars.FilmPlanetRepository;
 import br.com.starwars.api.external.repository.starwars.FilmRepository;
+import br.com.starwars.api.external.repository.starwars.FilmSpeciesRepository;
+import br.com.starwars.api.external.repository.starwars.FilmStarshipRepository;
+import br.com.starwars.api.external.repository.starwars.FilmVehicleRepository;
 import br.com.starwars.api.external.repository.starwars.PersonRepository;
+import br.com.starwars.api.external.repository.starwars.PersonSpeciesRepository;
+import br.com.starwars.api.external.repository.starwars.PersonStarshipRepository;
+import br.com.starwars.api.external.repository.starwars.PersonVehicleRepository;
+import br.com.starwars.api.external.repository.starwars.PlanetPersonRepository;
 import br.com.starwars.api.external.repository.starwars.PlanetRepository;
 import br.com.starwars.api.external.repository.starwars.SpeciesRepository;
 import br.com.starwars.api.external.repository.starwars.StarshipRepository;
 import br.com.starwars.api.external.repository.starwars.VehicleRepository;
 import br.com.starwars.api.external.repository.starwars.entities.FilmEntity;
+import br.com.starwars.api.external.repository.starwars.entities.FilmPersonEntity;
+import br.com.starwars.api.external.repository.starwars.entities.FilmPlanetEntity;
+import br.com.starwars.api.external.repository.starwars.entities.FilmSpeciesEntity;
+import br.com.starwars.api.external.repository.starwars.entities.FilmStarshipEntity;
+import br.com.starwars.api.external.repository.starwars.entities.FilmVehicleEntity;
 import br.com.starwars.api.external.repository.starwars.entities.PersonEntity;
+import br.com.starwars.api.external.repository.starwars.entities.PersonSpeciesEntity;
+import br.com.starwars.api.external.repository.starwars.entities.PersonStarshipEntity;
+import br.com.starwars.api.external.repository.starwars.entities.PersonVehicleEntity;
 import br.com.starwars.api.external.repository.starwars.entities.PlanetEntity;
+import br.com.starwars.api.external.repository.starwars.entities.PlanetPersonEntity;
 import br.com.starwars.api.external.repository.starwars.entities.SpeciesEntity;
 import br.com.starwars.api.external.repository.starwars.entities.StarshipEntity;
 import br.com.starwars.api.external.repository.starwars.entities.VehicleEntity;
@@ -29,6 +46,15 @@ public class RepositoryServiceImpl implements RepositoryService {
     private final SpeciesRepository speciesRepository;
     private final StarshipRepository starshipRepository;
     private final VehicleRepository vehicleRepository;
+    private final FilmPersonRepository filmPersonRepository;
+    private final FilmPlanetRepository filmPlanetRepository;
+    private final FilmSpeciesRepository filmSpeciesRepository;
+    private final FilmVehicleRepository filmVehicleRepository;
+    private final FilmStarshipRepository filmStarshipRepository;
+    private final PersonVehicleRepository personVehicleRepository;
+    private final PersonStarshipRepository personStarshipRepository;
+    private final PersonSpeciesRepository personSpeciesRepository;
+    private final PlanetPersonRepository planetPersonRepository;
 
     @Override
     public void insertAllFilms(List<FilmEntity> list) {
@@ -59,4 +85,51 @@ public class RepositoryServiceImpl implements RepositoryService {
     public void insertAllVehicles(List<VehicleEntity> list) {
         vehicleRepository.saveAll(list);
     }
+
+    @Override
+    public void insertAllFilmPerson(List<FilmPersonEntity> list) {
+        filmPersonRepository.saveAll(list);
+    }
+
+    @Override
+    public void insertAllFilmPlanet(List<FilmPlanetEntity> list) {
+        filmPlanetRepository.saveAll(list);
+    }
+
+    @Override
+    public void insertAllFilmSpecies(List<FilmSpeciesEntity> list) {
+        filmSpeciesRepository.saveAll(list);
+    }
+
+    @Override
+    public void insertAllFilmVehicle(List<FilmVehicleEntity> list) {
+        filmVehicleRepository.saveAll(list);
+    }
+
+    @Override
+    public void insertAllFilmStarship(List<FilmStarshipEntity> list) {
+        filmStarshipRepository.saveAll(list);
+    }
+
+    @Override
+    public void insertAllPersonVehicle(List<PersonVehicleEntity> list) {
+        personVehicleRepository.saveAll(list);
+    }
+
+    @Override
+    public void insertAllPersonStarship(List<PersonStarshipEntity> list) {
+        personStarshipRepository.saveAll(list);
+    }
+
+    @Override
+    public void insertAllPersonSpecies(List<PersonSpeciesEntity> list) {
+        personSpeciesRepository.saveAll(list);
+    }
+
+    @Override
+    public void insertAllPlanetPerson(List<PlanetPersonEntity> list) {
+        planetPersonRepository.saveAll(list);
+    }
+
+
 }
