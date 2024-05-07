83c3cb835d921654bbb188e29adca1fd9a74e1bc
Adaumir Costa <adaumircosta@gmail.com>
2024-05-07 03:49:47 -0400

    Adjustment tests

--- src/test/java/br/com/starwars/api/usecase/impl/UpdateFilmUseCaseImplTest.java
+++ src/test/java/br/com/starwars/api/usecase/impl/UpdateFilmUseCaseImplTest.java
@@ -1,15 +1,21 @@
 package br.com.starwars.api.usecase.impl;
 
+import br.com.starwars.api.exceptions.DataNotFoundException;
+import br.com.starwars.api.exceptions.DatabaseException;
 import br.com.starwars.api.external.repository.starwars.services.RepositoryService;
+import br.com.starwars.api.interceptor.dto.ErrorDto;
 import br.com.starwars.api.service.impl.GetFilmDataServiceImpl;
 import br.com.starwars.api.usecase.UpdateFilmUseCase;
 import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.orm.hibernate5.HibernateQueryException;
 import org.springframework.stereotype.Service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -17,6 +23,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UpdateFilmUseCaseImplTest {
@@ -34,7 +41,27 @@ class UpdateFilmUseCaseImplTest {
     void musntThrowExceptionWhenCalled(){
 
         doNothing().when(repository).updateFilm(anyInt(), anyString());
+        when(repository.existsId(anyInt())).thenReturn(Boolean.TRUE);
+
         assertDoesNotThrow(() -> useCase.execute(1, "teste filme"));
+
+        verify(repository, times(1)).existsId(anyInt());
         verify(repository, times(1)).updateFilm(anyInt(), anyString());
+    }
+
+    @Test
+    void shouldThrowDataNotFoundExceptionWhenEpisodeIdNotExists() {
+
+        when(repository.existsId(anyInt())).thenReturn(Boolean.FALSE);
+
+        assertThatThrownBy(() -> {
+            useCase.execute(1000, "teste filme");
+        }).isInstanceOf(DataNotFoundException.class)
+                .extracting("erros", InstanceOfAssertFactories.list(ErrorDto.class))
+                .extracting(ErrorDto::getTitle)
+                .contains("Id inválido");
+
+        verify(repository, times(1)).existsId(anyInt());
+        verify(repository, times(0)).updateFilm(anyInt(), anyString());
     }
 }
